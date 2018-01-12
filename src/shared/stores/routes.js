814febe17bdec0133340cad8ffc8058a0e880a00
Naresh Bhatia <naresh.archfirst@gmail.com>
2018-01-11 22:24:24 -0500

    Added guidance on how to sequence routes

--- src/shared/stores/routes.js
+++ src/shared/stores/routes.js
@@ -13,8 +13,12 @@ const checkForUserSignedIn = (fromState, toState, routerStore) => {
     }
 };
 
-// Note: The routes are matched from top to bottom.
-// Make sure they are sequenced in the order of priority.
+// Routes are matched from top to bottom. Make sure they are sequenced
+// in the order of priority. It is generally best to sort them by pattern,
+// prioritizing specific patterns over generic patterns (patterns with
+// one or more parameters). For example:
+//     /items
+//     /items/:id
 export const routes = [
     {
         name: 'home',
