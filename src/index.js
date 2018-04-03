6f2024e4d9d5d9c47562bee0c5d51e3bb41b9e28
Naresh Bhatia <naresh.archfirst@gmail.com>
2018-04-02 22:11:53 -0400

    Removed index.css
    It is not required.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useStrict } from 'mobx';
-import './index.css';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
 
